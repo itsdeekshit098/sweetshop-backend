ddfdece44651646b2d1d22fc920aebe24a6b9d7b
DEEKSHITH REDDY <itsdeekshit098@gmail.com>
2025-09-17 19:57:08 +0530

    chore: initialize Spring Boot project with Maven and PostgreSQL dependencies
    Set up basic project structure with Spring Boot, PostgreSQL, and security dependencies.

---
+++ src/test/java/com/Incubyte/sweetshop/SweetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Incubyte.sweetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
